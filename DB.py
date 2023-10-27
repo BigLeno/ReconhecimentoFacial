4f1295943aa0697e770cb78db4c91b94d0ba3523
Rutileno Gabriel <92885893+BigLeno@users.noreply.github.com>
2023-10-27 17:50:15 -0300

    Código funcional e refatorado!

--- DB.py
+++ DB.py
@@ -1,12 +1,8 @@
-from cv2 import (imread, cvtColor, COLOR_BGR2RGB, VideoCapture, resize, rectangle, putText, FONT_HERSHEY_SIMPLEX,
-                 imshow,
-                 waitKey, destroyAllWindows)
+# from cv2 import (imread, cvtColor, COLOR_BGR2RGB, VideoCapture, resize, rectangle, putText, FONT_HERSHEY_SIMPLEX,imshow,waitKey, destroyAllWindows)
+import cv2
 from face_recognition import (face_encodings, face_locations, compare_faces, face_distance)
 from os import (listdir, path)
 from numpy import argmin
-
-cap = VideoCapture(0)
-limite_distancia = 0.4
 
 
 class DB:
@@ -24,56 +20,108 @@ class DB:
 
     def get_img_and_name_general(self) -> None:
         for cl in listdir(DB.directory):
-            self.images.append(imread(f'{DB.directory}/{cl}'))
+            self.images.append(cv2.imread(f'{DB.directory}/{cl}'))
             self.names.append(path.splitext(cl)[0])
 
     def find_encodings(self) -> None:
         for image in self.images:
-            self.encode_list.append(face_encodings(cvtColor(image, COLOR_BGR2RGB))[0])
+            self.encode_list.append(face_encodings(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))[0])
 
 
-dataBase = DB()
+class FaceRecognitionSystem:
+    def __init__(self, database, distance_limit):
+        """
+        Inicializa o sistema de reconhecimento facial.
 
-while True:
-    success, img = cap.read()
-    imgS = cvtColor(resize(img, (0, 0), None, 0.25, 0.25), COLOR_BGR2RGB)
+        Args:
+            database: Um objeto que contém dados de referência para reconhecimento facial.
+            distance_limit: A distância limite para considerar uma correspondência.
+        """
+        self.dataBase = database
+        self.limite_distancia = distance_limit
+        self.cap = cv2.VideoCapture(0)  # Inicializa a câmera
+        cv2.namedWindow('Webcam')
 
-    facesCurFrame = face_locations(imgS)
-    encodeCurFrame = face_encodings(imgS, facesCurFrame)
+    def find_faces(self, img):
+        """
+        Encontra e codifica faces na imagem fornecida.
 
-    access_granted = False
+        Args:
+            img: A imagem na qual as faces serão detectadas e codificadas.
 
-    for encodeFace, faceLoc in zip(encodeCurFrame, facesCurFrame):
-        matches = compare_faces(dataBase.encode_list, encodeFace, limite_distancia)
-        distancia = face_distance(dataBase.encode_list, encodeFace)
-        match = argmin(distancia)
+        Returns:
+            Uma lista de tuplas, onde cada tupla contém a codificação da face e a localização da face na imagem.
+        """
+        images = cv2.cvtColor(cv2.resize(img, (0, 0), None, 0.25, 0.25), cv2.COLOR_BGR2RGB)
+        faces_cur_frame = face_locations(images)
+        encode_cur_frame = face_encodings(images, faces_cur_frame)
+        return list(zip(encode_cur_frame, faces_cur_frame))
 
-        if matches[match]:
-            nome = dataBase.names[match].upper()
-            print(nome)
-            access_granted = True  # Acesso liberado se houver uma correspondência
+    def process_frame(self, img):
+        """
+        Processa um quadro de vídeo em busca de faces correspondentes.
 
-        if distancia[match] <= limite_distancia:
-            top, right, bottom, left = faceLoc
-            top *= 4
-            right *= 4
-            bottom *= 4
-            left *= 4
+        Args:
+            img: O quadro de vídeo a ser processado.
 
-            rectangle(img, (left, top), (right, bottom), (0, 255, 0), 2)
-            putText(img, nome, (left, top - 10), FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+        Returns:
+            Uma flag indicando se o acesso foi concedido e o nome da pessoa reconhecida.
+        """
+        access_granted = False
+        nome = ""
 
-    if access_granted:
-        # Libere o acesso aqui (por exemplo, abra uma porta)
-        print("Acesso Liberado")
-    else:
-        # Negue o acesso aqui (por exemplo, exiba uma mensagem de negação)
-        print("Acesso Negado")
+        encodings_and_locations = self.find_faces(img)
 
-    imshow('Webcam', img)
+        for encodeFace, faceLoc in encodings_and_locations:
+            matches = compare_faces(self.dataBase.encode_list, encodeFace, self.limite_distancia)
+            distancia = face_distance(self.dataBase.encode_list, encodeFace)
+            match = argmin(distancia)
 
-    if waitKey(1) & 0xFF == ord('q'):
-        break
+            if matches[match]:
+                nome = self.dataBase.names[match].upper()
+                access_granted = True  # Acesso liberado se houver uma correspondência
 
-cap.release()
-destroyAllWindows()
+            if distancia[match] <= self.limite_distancia:
+                top, right, bottom, left = faceLoc
+                top *= 4
+                right *= 4
+                bottom *= 4
+                left *= 4
+
+                cv2.rectangle(img, (left, top), (right, bottom), (0, 255, 0), 2)
+                cv2.putText(img, nome, (left, top - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+
+        return access_granted, nome
+
+    def run(self):
+        """
+        Executa o sistema de reconhecimento facial em tempo real.
+
+        Pressione 'q' para encerrar a execução.
+        """
+        while True:
+            success, img = self.cap.read()
+
+            access_granted, nome = self.process_frame(img)
+
+            if access_granted:
+                # Libere o acesso aqui (por exemplo, abra uma porta)
+                print("Acesso Liberado")
+            else:
+                # Negue o acesso aqui (por exemplo, exiba uma mensagem de negação)
+                print("Acesso Negado")
+
+            cv2.imshow('Webcam', img)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        self.cap.release()
+        cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    limite_distancia = 0.4
+    myDatabase = DB()
+    myFaceRecognitionSystem = FaceRecognitionSystem(myDatabase, limite_distancia)
+    myFaceRecognitionSystem.run()
