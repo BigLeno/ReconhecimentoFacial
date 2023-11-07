69e93cfc2c8041129044301c4c4c6e4b1c8cc70f
Ian da Hora <121323237+iandahora@users.noreply.github.com>
2023-11-07 20:13:02 -0300

    Refatorando

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ from numpy import argmin
 from string import ascii_lowercase
 from random import choice
 import pandas as pd
-from time import strftime
+from datetime import datetime, timedelta
+
+csv_directory = "registro_iteracoes.csv"
+data_hora = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 
 class DB:
@@ -16,10 +19,10 @@ class DB:
 
     def __init__(self) -> None:
         print("Iniciando o sistema... \nCarregando o Banco de Dados... ")
-        self.images, self.names = [], []
+        self.images, self.names= [], []
         self.get_img_and_name_general()
         print("Banco carregado com sucesso... \nIniciando o encoding das imagens...")
-        self.encode_list = []
+        self.encode_list =  []
         self.find_encodings()
         print("Encoding terminado com sucesso... \nSistema iniciado com sucesso")
 
@@ -45,8 +48,8 @@ class FaceRecognitionSystem:
             Inicializa o sistema de reconhecimento facial.
         """
         self.dataBase = database
-        self.unknown_faces_data = pd.DataFrame(columns=["ID", "Timestamp"])
         self.limite_distancia = distance_limit
+        self.unknown_faces_seen_at = {}
         self.cap = VideoCapture(0)  # Inicializa a câmera
         self.cap.set(3, 640)  # Define a largura para 640 pixels (VGA)
         self.cap.set(4, 480)  # Define a altura para 480 pixels (VGA)
@@ -58,17 +61,22 @@ class FaceRecognitionSystem:
 
     @staticmethod
     def find_faces(img):
-        """
-            Encontra e codifica faces na imagem fornecida.
-        """
         images = cvtColor(resize(img, (0, 0), None, 0.25, 0.25), COLOR_BGR2RGB)
         faces_cur_frame = face_locations(images)
         encode_cur_frame = face_encodings(images, faces_cur_frame)
         return list(zip(encode_cur_frame, faces_cur_frame))
+    
+    @staticmethod
+    def save_img(directory, img, archive_name="sem_nome"):
+        makedirs(directory, exist_ok=True)
+        imwrite(f"{directory}/{archive_name}.jpg", img)
 
     def process_frame(self, img):
         access_granted = False
         nome = ""
+        
+        current_time = datetime.now()
+        time_delay = 5
 
         encodings_and_locations = self.find_faces(img)
 
@@ -90,27 +98,49 @@ class FaceRecognitionSystem:
                 rectangle(img, (left, top), (right, bottom), (0, 255, 0), 2)
                 putText(img, nome, (left, top - 10), FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
             else:
-                print("Rosto desconhecido salvo!")
+                data = pd.read_csv(csv_directory)
                 unique_id = self.generate_unique_id()
-                new_entry = pd.DataFrame({"ID": [unique_id], "Timestamp": [strftime("%Y-%m-%d %H:%M:%S")]})
-                self.unknown_faces_data = pd.concat([self.unknown_faces_data, new_entry], ignore_index=True)
+
+                last_key = list(self.unknown_faces_seen_at.keys())[-1] if self.unknown_faces_seen_at else None
+                last_seen = self.unknown_faces_seen_at.get(last_key, None)
+                
+                unknown_face = img[top:bottom, left:right]
+                rectangle(img, (left, top), (right, bottom), (0, 0, 255), 2)
+
+                if last_seen is None or (current_time - last_seen).total_seconds() >= time_delay:
+                    print("Rosto desconhecido encontrado")
+                    
+                    data_hora = current_time.strftime("%Y-%m-%d %H:%M:%S")
+                    data.loc[len(data)] = [data_hora, "Não reconhecido", f'RD/{unique_id}']
+                    
+                    self.save_img("RD", unknown_face, unique_id)
+                    self.unknown_faces_seen_at[unique_id] = current_time
+                    data.to_csv(csv_directory, index=False)
+                    print("Acesso registrado!")
+
 
         return access_granted, nome
 
     def run(self):
-        """
-        Executa o sistema de reconhecimento facial em tempo real.
+        timer = 5  # Defina o tempo de atraso (em segundos) conforme necessário
+        last_access_time = datetime.now() - timedelta(seconds=timer)  # Inicialize com um tempo que permitirá a primeira ação
 
-        Pressione 'q' para encerrar a execução.
-        """
         while True:
             success, img = self.cap.read()
 
+            current_time = datetime.now()
+            time_elapsed = (current_time - last_access_time).total_seconds()
             access_granted, nome = self.process_frame(img)
 
             if access_granted:
-                # Libere o acesso aqui (por exemplo, abra uma porta)
-                print("Acesso Liberado")
+                if time_elapsed >= timer:
+                    print(f"Seja bem-vindo {nome}, acesso liberado!")
+                    data_hora = current_time.strftime("%Y-%m-%d %H:%M:%S")
+                    data = pd.read_csv(csv_directory)
+                    data.loc[len(data)] = [data_hora, "Reconhecido", f'DB/{nome.lower()}.jpg']
+                    data.to_csv(csv_directory, index=False)
+                    last_access_time = current_time  
+                    print("Acesso registrado!")
 
             imshow('Webcam', img)
 
