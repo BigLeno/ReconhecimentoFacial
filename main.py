3d55fcc6d35328dc550aa1d30f64f42887c17d51
Rutileno Gabriel <r.gabriel530@outlook.com>
2023-11-11 01:45:22 -0300

    Funcionalidades
    Definir qualidade da webcam

--- main.py
+++ main.py
@@ -33,6 +33,7 @@ class FaceRecognitionSystem:
         self.dataBase, self.limite_distancia = DB(), distance_limit
         self.unknown_faces_seen_at, self.encode_list= {}, []
         self.colors = {'red':(0,0,255), 'green':(0,255,0), 'blue':(255,0,0)}
+        self.quality_settings = {'qvga': (320, 240),'vga': (640, 480),'hd': (1280, 720),'full_hd': (1920, 1080)}
         self.access_types = ["Reconhecido", "Não reconhecido"]
         self.date_and_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         self.csv_directory = "registro_iteracoes.csv"
@@ -42,10 +43,13 @@ class FaceRecognitionSystem:
         self.getWebcam()
         print("Sistema iniciado sem falhas")
 
-    def getWebcam(self) -> None:
-        self.cap = VideoCapture(0)  # Inicializa a câmera
-        self.cap.set(3, 640)  # Define a largura para 640 pixels (VGA)
-        self.cap.set(4, 480)  # Define a altura para 480 pixels (VGA)
+    def getWebcam(self, quality: Optional[str] = 'vga') -> None:
+        self.cap = VideoCapture(0)
+        if quality not in self.quality_settings:
+            print(f"Qualidade de webcam não suportada: {quality}")
+            return
+        self.cap.set(3, self.quality_settings[quality][0])
+        self.cap.set(4, self.quality_settings[quality][1])
         namedWindow('Webcam')
     
     def find_encodings(self) -> None:
@@ -53,7 +57,7 @@ class FaceRecognitionSystem:
             self.encode_list = pool.map(self.encode_face, self.dataBase.images)
 
     @staticmethod
-    def save_img(directory, img, archive_name="sem_nome") -> None:
+    def save_img(directory, img, archive_name:Optional[str]="sem_nome") -> None:
         makedirs(directory, exist_ok=True)
         imwrite(f"{directory}/{archive_name}.jpg", img)
 
@@ -62,7 +66,7 @@ class FaceRecognitionSystem:
         data.loc[len(data)] = [self.date_and_time, self.access_types[acess_type], f'{relative_path}/{name.lower()}.jpg']
         data.to_csv(self.csv_directory, index=False)
     
-    def put_rectangles_and_text(self, image, positions:tuple, color:str, text='') -> None:
+    def put_rectangles_and_text(self, image, positions:tuple, color:str, text:Optional[str]='') -> None:
         rectangle(image, (positions[3], positions[0]), (positions[1], positions[2]), self.colors[color], 2)
         putText(image, text, (positions[3], positions[0]), FONT_HERSHEY_SIMPLEX, 0.9, self.colors[color], 2)
 
@@ -126,7 +130,7 @@ class FaceRecognitionSystem:
         return access_granted, nome
     
     def run(self):
-        timer = 5  # Defina o tempo de atraso (em segundos) conforme necessário
+        timer = 5  
         last_access_time = datetime.now() - timedelta(seconds=timer)  # Inicialize com um tempo que permitirá a primeira ação
 
         while True:
