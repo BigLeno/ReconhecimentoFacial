3b04604fc75489b4bc3f81802a7f6c41a79e0c93
Rutileno Gabriel <r.gabriel530@outlook.com>
2023-11-11 02:44:30 -0300

    Documentando
    Documentando método a método do código

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from string import ascii_lowercase
 from random import choice
 import pandas as pd
 from datetime import datetime, timedelta
-from typing import Tuple, Optional, List
+from typing import Tuple, Optional, List, Union
 
 class DB:
     def __init__(self, db_directory='DB') -> None:
@@ -18,6 +18,7 @@ class DB:
         self.get_img_and_name_general()
         
     def get_img_and_name_general(self) -> None:
+        """Carrega imagens e nomes do banco de dados."""
         print("Carregando o Banco de Dados... ")
         for cl in listdir(self.db_directory):
             self.images.append(imread(f'{self.db_directory}/{cl}'))
@@ -44,6 +45,7 @@ class FaceRecognitionSystem:
         print("Sistema iniciado sem falhas")
 
     def getWebcam(self, quality: Optional[str] = 'vga') -> None:
+        """Inicializa a webcam com a qualidade especificada"""
         self.cap = VideoCapture(0)
         if quality not in self.quality_settings:
             print(f"Qualidade de webcam não suportada: {quality}")
@@ -53,33 +55,45 @@ class FaceRecognitionSystem:
         namedWindow('Webcam')
     
     def find_encodings(self) -> None:
+        """Gera as codificações faciais para as imagens no banco de dados"""
         with Pool(processes=None) as pool:
             self.encode_list = pool.map(self.encode_face, self.dataBase.images)
 
     @staticmethod
     def save_img(directory, img, archive_name:Optional[str]="sem_nome") -> None:
+        """Salva uma imagem em um diretório específico com um nome de arquivo opcional"""
         makedirs(directory, exist_ok=True)
         imwrite(f"{directory}/{archive_name}.jpg", img)
 
     def register_acess(self, relative_path:str, acess_type:int,  name:str) -> None:
+        """Registra um acesso no banco de dados com informações de tempo, tipo e nome"""
         data = pd.read_csv(self.csv_directory)
         data.loc[len(data)] = [self.date_and_time, self.access_types[acess_type], f'{relative_path}/{name.lower()}.jpg']
         data.to_csv(self.csv_directory, index=False)
     
     def put_rectangles_and_text(self, image, positions:tuple, color:str, text:Optional[str]='') -> None:
+        """Adiciona retângulo e texto a uma imagem na posição especificada"""
         rectangle(image, (positions[3], positions[0]), (positions[1], positions[2]), self.colors[color], 2)
         putText(image, text, (positions[3], positions[0]), FONT_HERSHEY_SIMPLEX, 0.9, self.colors[color], 2)
 
-    def get_file_count(self):
+    def get_file_count(self) -> Union[int, int]:
+        """
+            Retorna o número de arquivos no diretório do banco de dados, se existir.
+            Retorna 0 se o diretório não existir ou não for um diretório válido.
+        """
         if path.exists(self.dataBase.db_directory) and path.isdir(self.dataBase.db_directory):
             files = listdir(self.dataBase.db_directory)
             return len(files)
         else:
             return 0
     
-    def monitor_directory(self):
-       current_file_count = self.get_file_count()
-       if current_file_count != self.last_file_count:
+    def monitor_directory(self) -> None:
+        """
+            Monitora o diretório em busca de alterações no número de arquivos.
+            Quando a pasta é atualizada, recarrega o banco de dados e recria as codificações.
+        """
+        current_file_count = self.get_file_count()
+        if current_file_count != self.last_file_count:
             print("\n---------------------------\n")
             print("\nA pasta foi atualizada...")
             self.dataBase = DB()
@@ -91,16 +105,19 @@ class FaceRecognitionSystem:
 
     @staticmethod
     def generate_unique_id(length=8) -> str:
+        """Gera um ID único aleatório com o comprimento especificado"""
         return ''.join(choice(ascii_lowercase) for _ in range(length))
     
     @staticmethod
     def encode_face(image) -> Optional[List]:
+        """Codifica o rosto em uma imagem e retorna a codificação, ou None se não for possível"""
         encoding = face_encodings(cvtColor(image, COLOR_BGR2RGB))
         if encoding:
             return encoding[0]
     
     @staticmethod
     def find_faces(img) -> List[Tuple[list, list]]:
+        """Encontra e retorna as codificações e localizações dos rostos em uma imagem."""
         return list(zip(
             face_encodings(
                 cvtColor(resize(img, (0, 0), None, 0.25, 0.25), COLOR_BGR2RGB), 
@@ -109,6 +126,7 @@ class FaceRecognitionSystem:
             ))
     
     def compare_faces_and_get_distances(self, data, new_faces) -> Tuple[list, list, int]:
+        """Compara as faces e obtém as distâncias entre elas."""
         return(
             compare_faces(data, new_faces, self.limite_distancia),
             face_distance(data, new_faces), 
@@ -116,6 +134,10 @@ class FaceRecognitionSystem:
         )
 
     def process_frame(self, img) -> Tuple[bool, str]:
+        """
+            Processa um frame de imagem para reconhecimento facial e 
+            retorna se o acesso foi concedido e o nome, se reconhecido.
+        """
         access_granted = False
         nome = ""
         
@@ -148,7 +170,8 @@ class FaceRecognitionSystem:
 
         return access_granted, nome
     
-    def run(self):
+    def run(self) -> None:
+        """Inicia o sistema de reconhecimento facial e controla a webcam em um loop contínuo."""
         timer = 5  
         last_access_time = datetime.now() - timedelta(seconds=timer)  # Inicialize com um tempo que permitirá a primeira ação
 
